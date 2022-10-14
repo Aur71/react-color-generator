84ff2c8782799c854a7dd786572a24fcd05112b4
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-10-14 12:14:50 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState, useEffect, useCallback } from 'react';
+import Item from './Item';
+import Values from 'values.js';
+
+const App = () => {
+  const [color, setColor] = useState('');
+  const [colors, setColors] = useState(new Values('rgb(71,17,71)').all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setColors(new Values(color).all(10));
+  };
+
+  const handleRandom = () => {
+    var randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    setColor(`#${randomColor}`);
+  };
+
+  return (
+    <main>
+      <header>
+        <h1>Color Generator</h1>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="color"
+            value={color}
+            placeholder="rgb(71,17,71)"
+            onChange={(e) => setColor(e.target.value)}
+          />
+          <button>Submit</button>
+          <button onClick={handleRandom}>Random</button>
+        </form>
+      </header>
+
+      <section>
+        {colors.map((color, index) => {
+          return <Item key={index} color={color} index={index} />;
+        })}
+      </section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/Item.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+
+const Item = ({ color, index }) => {
+  const [alert, setAlert] = useState(false);
+  const { rgb, weight } = color;
+  const clr = index > 10;
+  const rgbColor = `rgb(${rgb.join(',')})`;
+
+  const handleClick = () => {
+    setAlert(true);
+    navigator.clipboard.writeText(rgbColor);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setAlert(false);
+    }, 3000);
+
+    return () => clearTimeout(timeout);
+  }, [alert]);
+
+  return (
+    <div
+      style={{ backgroundColor: rgbColor }}
+      className={`grid-item ${index > 10 && 'white'}`}
+      onClick={handleClick}
+    >
+      <p>{rgbColor}</p>
+      <p>{weight}%</p>
+      {alert && <p>copied to clipboard</p>}
+    </div>
+  );
+};
+
+export default Item;
